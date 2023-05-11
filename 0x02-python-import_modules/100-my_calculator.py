fac6c0388f549ee860de3dea9cdff678dd4814be
your name <you@example.com>
2023-05-11 15:26:27 -0700

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     opts = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(opts.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
