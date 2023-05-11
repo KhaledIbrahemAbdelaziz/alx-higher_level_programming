dbd10042f057b42615e1387a6ad40a8c168ee21c
your name <you@example.com>
2023-05-11 14:04:50 -0700

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print the sum, difference, multiply and division of 10 and 5"""
+    """print the sum, difference, multiply and division of 10 and 5
+    """
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
+    
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
