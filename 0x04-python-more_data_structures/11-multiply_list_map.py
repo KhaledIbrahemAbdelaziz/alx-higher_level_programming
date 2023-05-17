de6d5bb897276c883db697d0927283579db6b787
your name <you@example.com>
2023-05-17 16:49:38 -0700

    Task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,5 @@
 def multiply_list_map(my_list=[], number=0):
     """returns a list with all values multiplied
     by a number without using any loops."""
-    return (list(map((lambda x: x * number), my_list)))
+    new_list = my_list.copy()
+    return (list(map((lambda x: x * number), new_list)))
