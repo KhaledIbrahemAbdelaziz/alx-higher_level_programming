8fc3f0090bf1ead17bb6c42aa966ae49eece7ed4
your name <you@example.com>
2023-05-17 15:46:36 -0700

    Task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    """returns the number of keys in a dictionary."""
+    """returns the number of keys
+    in a dictionary."""
     num = 0
     lists = list(a_dictionary.keys())
 
