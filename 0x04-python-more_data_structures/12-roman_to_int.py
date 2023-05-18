db2e2fae5678c66a85dc1512581c8a04a078a0db
your name <you@example.com>
2023-05-17 17:36:33 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,6 @@ def to_subtract(list_num):
         if max_list > n:
             to_sub += n
     return (max_list - to_sub)
-
 def roman_to_int(roman_string):
     """converts a Roman numeral to an integer."""
     if not roman_string:
