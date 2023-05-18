bee2d0c93c81ef389d1bb9e8cd604377ca6746a6
your name <you@example.com>
2023-05-17 17:35:45 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def to_subtract(list_num):
+    """To subtract"""
     to_sub = 0
     max_list = max(list_num)
-
     for n in list_num:
         if max_list > n:
             to_sub += n
