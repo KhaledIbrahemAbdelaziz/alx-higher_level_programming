2bd2c5108f8dc5c97c1ca1529b9ba62495437671
your name <you@example.com>
2023-05-30 12:16:31 -0700

    Task 1 Updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Define class square"""
+
+
 class Square:
     """Representing a square"""
+    
     def __init__(self, size):
         """initialize the size of the new square.
         args:
