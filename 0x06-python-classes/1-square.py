ab9c6adb952c0b51eb69eb7700f79578687cdf1b
your name <you@example.com>
2023-05-30 10:14:29 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class square"""
+class Square:
+    """Representing a square"""
+    def __init__(self, size):
+        """initialize the size of the new square.
+        args:
+        size (int): the private size of the square"""
+        self.__size = size
