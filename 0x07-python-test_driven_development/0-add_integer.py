208c6d1fb0ba65c2dfac9fc8b6143cfb600ede35
your name <you@example.com>
2023-06-03 17:08:59 -0700

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 # 0-add_integer.py
-"""define addition function of integers"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """adds 2 integers a and b
-    float arguments must be casted to integer first before addition
-    Errors raise:
-    TypeError: if a and b is not integer or float"""
+     """Return the integer addition of a and b.
+
+    Float arguments are typecasted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
