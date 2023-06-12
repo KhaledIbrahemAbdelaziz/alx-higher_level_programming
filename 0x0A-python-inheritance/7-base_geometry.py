14ddc0c425a0e554481258bbe5b54f7de7fa4d82
your name <you@example.com>
2023-06-12 13:02:54 -0700

    Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
         ValueError: <name> must be greater than 0.
         """
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("<name> must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("<name> must be greater than 0".format(name))
