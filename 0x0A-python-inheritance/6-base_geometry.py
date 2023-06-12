a218cbe6179b710bdfa57c26524d79b299fc3b9c
your name <you@example.com>
2023-06-12 12:31:27 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define the base geometry class"""
+
+
+class BaseGeometry:
+    """Representing the base geometry"""
+
+    def area(self):
+        """that raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
