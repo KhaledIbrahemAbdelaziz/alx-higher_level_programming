10be9489a1fc1027998bb8dc262fd3186101836b
your name <you@example.com>
2023-06-13 15:53:28 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Define the add_item function"""
+"""
+    This is a python script that adds all arguments to a Python List.
+    List is then saved to a file.
+"""
+
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
+
     filename = "add_item.json"
+
     try:
-        l = load_from_json_file(filename)
+        arg_list = load_from_json_file(filename)
     except:
-        l = []
-    for i in sys.argv[1:]:
-        l.append(i)
-    save_to_json_file(l, filename)
+        arg_list = []
+
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    save_to_json_file(arg_list, filename)
