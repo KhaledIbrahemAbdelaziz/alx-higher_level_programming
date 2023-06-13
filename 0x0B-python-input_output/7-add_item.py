f4881d6b54f3a8eed96c2ad0e030d6e6c93a6fc1
your name <you@example.com>
2023-06-13 15:51:06 -0700

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define the add_item function"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+
+    filename = "add_item.json"
+    try:
+        l = load_from_json_file(filename)
+    except:
+        l = []
+    for i in sys.argv[1:]:
+        l.append(i)
+    save_to_json_file(l, filename)
