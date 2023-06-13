618aa436d9923c333cfc6639b14f6ed3585454b2
your name <you@example.com>
2023-06-13 15:52:13 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
-
     filename = "add_item.json"
     try:
         l = load_from_json_file(filename)
