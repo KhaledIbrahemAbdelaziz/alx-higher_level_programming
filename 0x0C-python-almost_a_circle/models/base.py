d9756351d4af37513bd8ef4dced7c607e52dd3a4
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-06-17 17:57:08 -0700

    Task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,14 @@ class Base:
             return []
         else:
             return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                inst = cls(1, 1)
+            else:
+                inst = cls(1)
+            inst.update(**dictionary)
+            return (inst)
