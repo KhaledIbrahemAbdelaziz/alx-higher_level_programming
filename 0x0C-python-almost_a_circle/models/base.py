9a96c0b1a832d0030aaa9af4739bdc7410146ec7
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-06-19 16:16:07 -0700

    Task 0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,8 +21,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
