005436249533bff95cba7bd80e149be4d4da12c2
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-06-19 13:38:46 -0700

    Task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,35 +25,41 @@ class Base:
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation 
         of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or 
+        list_dictionaries == []:
             return "[]"
         else:
             return (json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs 
+        """writes the JSON string representation 
+        of list_objs 
         to a file"""
         filename = cls.__name__ + ".json"
         with open(filename, "w") as j:
             if list_objs is None:
                 j.write("[]")
             else:
-                list_dics = [i.to_dictionary() for i in list_objs]
+                list_dics = [i.to_dictionary() 
+                        for i in list_objs]
                 j.write(Base.to_json_string(list_dics))
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of the JSON string representation 
+        """returns the list of the JSON string 
+        representation 
         json_string"""
-        if json_string is None or json_string == "[]":
+        if json_string is None or 
+        json_string == "[]":
             return []
         else:
             return (json.loads(json_string))
 
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
+        """returns an instance with all attributes 
+        already set"""
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 inst = cls(1, 1)
