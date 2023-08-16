86ba9bc7406c3e81006add5dee8144940d621b43
your name <you@example.com>
2023-08-16 04:09:57 -0700

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-	console.log('Missing size');
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
 } else {
-	const num = Number(process.argv[2]);
-	for (let i = 0; i < num; i++) {
-		console.log('X'.repeat(num));
-	}
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
