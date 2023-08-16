eb642fb13cb7492ebb13489537df21c444c149af
your name <you@example.com>
2023-08-16 03:53:03 -0700

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let line = 0; line < arr.length; line++) {
-	console.log(arr[line]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
