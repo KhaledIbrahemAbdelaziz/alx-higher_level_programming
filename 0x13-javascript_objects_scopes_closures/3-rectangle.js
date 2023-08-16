54b0145ce90b2e7ee06b8b508740a189ebfd1775
your name <you@example.com>
2023-08-16 06:37:45 -0700

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let rect = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
-        rect += 'X';
+        s += 'X';
       }
-      console.log(rect);
+      console.log(s);
     }
   }
 }
