53f842ac54a7fda0c4ad35810e1e7112f15e360f
your name <you@example.com>
2023-08-18 07:41:25 -0700

    Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""changes the name of a State object from the database hbtn_0e_6_usa"""
+"""changes the name of a State object from
+the database hbtn_0e_6_usa"""
 
-import sys
-from model_state import State
-from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker
+if __name__ == "__main__":
 
-if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    newnamestate = session.query(State).filter(State.id == 2).first()
-    newnamestate.name = 'New Mexico'
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
     session.commit()
     session.close()
