6916858f04e714796cc4c680e9c50f6fe4d94798
your name <you@example.com>
2023-08-18 05:32:02 -0700

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """lists all State objects from the database hbtn_0e_6_usa"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import sys
     from model_state import Base, State
@@ -9,9 +9,8 @@ if __name__ == '__main__':
     from sqlalchemy.orm import Session
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-            .format(sys.argv[1], sys.argv[2], 
-                sys.argv[3]), pool_pre_ping=True)
-    
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
